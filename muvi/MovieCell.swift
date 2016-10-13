86e190d68ca946f710688699321d3a23db104494
elai <minzeycat@gmail.com>
2016-10-13 00:22:24 -0700

    initial commit

---
+++ muvi/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  muvi
+//
+//  Created by Estella Lai on 10/12/16.
+//  Copyright © 2016 Estella Lai. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    // important to not call this imageView
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
